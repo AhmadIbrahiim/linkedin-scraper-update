fe135cd2c513ef66730e49a2150bbb8712d3af97
Ahmed Ibrahim <Ahmed.Ibrahhim@Outlook.com>
2020-11-18 17:14:35 +0200

    Update tm.js

--- tm.js
+++ tm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.tampermonkey.net/
