1d332e630810e3f3627d0c5a3d746a31ca59ec8b
Ahmed Ibrahim <Ahmed.Ibrahhim@Outlook.com>
2020-11-18 18:34:23 +0200

    Update tm.js

--- tm.js
+++ tm.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript V 0.6
+// @name         New Userscript V 0.7
 // @namespace    http://tampermonkey.net/
 // @updateURL    https://github.com/AhmadIbrahiim/linkedin-scraper-update/raw/master/tm.js
 // @downloadURL  https://github.com/mygithubaccount/linkedin-scraper-update/raw/master/tm.js
-// @version      0.6
+// @version      0.7
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.tampermonkey.net/
@@ -14,5 +14,5 @@
     'use strict';
 
     // Your code here...
-   alert("Hello world You are awesome");
+   alert("Hello world You are awesome......");
 })();
